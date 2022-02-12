11e4455abde4db3056f680d9e2b0beac97deee25
VladimirIzRossii <antvg05@yandex.ru>
2022-02-12 14:22:50 +0300

    end date added

--- test.py
+++ test.py
@@ -22,16 +22,17 @@ for i in quotes:
         names.append(i.text)
 for i in dat:
     if 'title' in str(i):
-        time = i.text[:-1].split('T')
-        datet = list(map(int, time[0].split('-')))
-        my_date = dt.date(datet[0], datet[1], datet[-1])
-        datet = list(map(int, time[1].split(':')))
-        my_time = dt.time(datet[0], datet[1], datet[-1])
-        my_datetime = dt.datetime.combine(my_date, my_time)
-        delta_time1 = dt.timedelta(hours=3)
-        dates.append(my_datetime + delta_time1)
+        ong = ''
     else:
-        dates.append('Ongoing')
+        ong = 'Ongoing, ends: '
+    time = i.text[:-1].split('T')
+    datet = list(map(int, time[0].split('-')))
+    my_date = dt.date(datet[0], datet[1], datet[-1])
+    datet = list(map(int, time[1].split(':')))
+    my_time = dt.time(datet[0], datet[1], datet[-1])
+    my_datetime = dt.datetime.combine(my_date, my_time)
+    delta_time1 = dt.timedelta(hours=3)
+    dates.append(ong + str(my_datetime + delta_time1))
 for i in lin:
     links.append(str(i).split('a href="')[1].split('"')[0])
 links = links[::2]
