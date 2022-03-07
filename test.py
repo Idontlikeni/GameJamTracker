350c2d9307e9e586f8bc5d6f56ab4cb80afcd885
VladimirIzRossii <antvg05@yandex.ru>
2022-03-07 19:42:42 +0300

    ver1

--- test.py
+++ test.py
@@ -8,6 +8,7 @@ dates = []
 links = []
 htm = ''
 images = []
+joined = []
 url = 'https://itch.io/jams'
 response = requests.get(url)
 soup = BeautifulSoup(response.text, 'lxml')
@@ -15,6 +16,7 @@ quotes = soup.find_all('div', class_="conversion_link_widget")
 dat = soup.find_all('span', class_="date_countdown")
 imag = soup.find_all('div', class_='jam_cover')
 lin = soup.find_all(['div'], class_="conversion_link_widget")
+jo = soup.find_all(['div'], class_="stat")
 for i in imag:
     images.append(str(i).split('data-background_image="')[1].split('"')[0])
 for i in quotes:
@@ -36,6 +38,8 @@ for i in dat:
 for i in lin:
     links.append(str(i).split('a href="')[1].split('"')[0])
 links = links[::2]
+for i in jo:
+    joined.append(i.text.split()[0])
 
 for i in range(len(names)):
     htm += f'</br><br>{names[i]}</br><img src="{images[i]}"></img></br>{dates[i]}</br><a href="https://itch.io{links[i]}">https://itch.io{links[i]}</a>'
@@ -47,21 +51,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def qwertyj():
-    return f'''<!doctype html>
-                <html lang="en">
-                  <head>
-                    <meta charset="utf-8">
-                    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-                    <link rel="stylesheet"
-                    href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
-                    integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
-                    crossorigin="anonymous">
-                    <title>123</title>
-                  </head>
-                  <body>
-                    {htm}
-                  </body>
-                </html>'''
+    return
 
 
 if __name__ == '__main__':
